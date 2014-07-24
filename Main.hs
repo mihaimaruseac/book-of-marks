c2a0e3aad22c37f0586053565ce250333abcc43f
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-07-24 19:45:45 -0400

    Simple test instance.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,17 @@
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+import Control.Applicative
+import Data.SafeCopy
+import Data.ByteString
+
+type URI = ByteString
+type Tag = ByteString
+type Description = ByteString
+data Test = T URI [Tag] Description deriving Show
+
+instance SafeCopy Test where
+  putCopy (T uri tags desc) = contain $ safePut uri >> safePut tags >> safePut desc
+  getCopy = contain $ T <$> safeGet <*> safeGet <*> safeGet
 
 main = undefined
