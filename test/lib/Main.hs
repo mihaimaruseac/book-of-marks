b0e126fc8a5fb26e1f7e84bb441419ce9396cf56
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-08-02 17:08:09 -0400

    Add some tests.

--- test/lib/Main.hs
+++ test/lib/Main.hs
@@ -1,1 +1,31 @@
-main = print "This is a test!"
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.SmallCheck
+
+import Data.List (sort)
+
+main = defaultMain $ testGroup "library-tests" tests
+
+tests =
+  [ testGroup "SmallCheck" smallCheckTests
+  , testGroup "Unit tests" unitTests
+  ]
+
+smallCheckTests =
+  [ testProperty "sort == sort . reverse" $ prop_sort
+  , testProperty "Fermat's last theorem" $ prop_fermat
+  ]
+
+unitTests =
+  [ testCase "Lists of different length" $ case_diff_len
+  , testCase "Lists of same length" $ case_same_len
+  ]
+
+prop_sort :: [Int] -> Bool
+prop_sort l = sort l == sort (reverse l)
+
+prop_fermat :: Monad m => Integer -> Integer -> Integer -> Integer -> Property m
+prop_fermat x y z n = n >= 3 ==> x^n + y^n /= z^n
+
+case_diff_len = [1, 2, 3] `compare` [1, 2] @?= GT
+case_same_len = [1, 2, 3] `compare` [1, 2, 2] @?= LT
