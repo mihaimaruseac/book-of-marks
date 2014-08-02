6943996ce371fd28f5d2b223b8049adceee9c2b2
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-08-02 17:30:21 -0400

    Test for doctest.

--- src/BookmarkManager/Types.hs
+++ src/BookmarkManager/Types.hs
@@ -1,1 +1,11 @@
 module BookmarkManager.Types where
+
+{-|
+ - Small test function to test the doctest support.
+ -
+ - >>> testDocTest 41
+ - 42
+ - >>> last . take it . iterate testDocTest $ 1
+ - 42
+ -}
+testDocTest x = x + 1
