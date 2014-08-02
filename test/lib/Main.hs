f73524eaa89f3bc583faaa712664beebbbfa7c6d
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-08-02 17:13:53 -0400

    Make tests pass.

--- test/lib/Main.hs
+++ test/lib/Main.hs
@@ -25,7 +25,7 @@ prop_sort :: [Int] -> Bool
 prop_sort l = sort l == sort (reverse l)
 
 prop_fermat :: Monad m => Integer -> Integer -> Integer -> Integer -> Property m
-prop_fermat x y z n = n >= 3 ==> x^n + y^n /= z^n
+prop_fermat x y z n = and [x > 0, y > 0, z > 0, n >= 3] ==> x^n + y^n /= z^n
 
 case_diff_len = [1, 2, 3] `compare` [1, 2] @?= GT
-case_same_len = [1, 2, 3] `compare` [1, 2, 2] @?= LT
+case_same_len = [1, 2, 3] `compare` [1, 2, 2] @?= GT
